665c3958d983008f62b3ffb37f0d4628e3ecd477
Jussi Latvaniemi <jussi.latvaniemi@gmail.com>
2019-10-06 02:33:12 +0300

    refactored table to a prototype

--- src/table.js
+++ src/table.js
@@ -1,15 +1,16 @@
-import Block from './block';
+const table = (function creator() {
+  function Table() { }
 
-const table = {
-  class: 'minesweeper__table',
-  build: function buildTable(width, height) {
+  Table.prototype.class = 'minesweeper__table';
+
+  Table.prototype.build = function buildTable(Block, width, height) {
     const domTable = document.createElement('TABLE');
 
     for (let i = 0; i < height; i += 1) {
       const row = document.createElement('tr');
 
       for (let j = 0; j < width; j += 1) {
-        row.appendChild(new Block().element);
+        row.appendChild(new Block(this.game).element);
       }
 
       row.classList.add('minesweeper__row');
@@ -18,7 +19,9 @@ const table = {
 
     domTable.classList.add(this.class);
     return domTable;
-  },
-};
+  };
+
+  return Table;
+}());
 
 export default table;
