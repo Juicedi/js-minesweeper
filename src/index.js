c56cad83a998e4c4b9f9c35a6f1a2b817d043fba
Jussi Latvaniemi <jussi.latvaniemi@gmail.com>
2019-12-15 22:36:52 +0200

    fixed some texts and made play area bigger

--- src/index.js
+++ src/index.js
@@ -4,21 +4,33 @@ import blockGridConstructor from './block';
 
 const element = document.createElement('DIV');
 const table1 = new Table();
+
+const gameState = {
+  end: 0,
+  running: 1,
+};
+
+const gameText = {
+  start: 'Click blocks and try to avoid mines.',
+  lose: 'You have lost the game.',
+  win: 'You have won the game!',
+};
+
 const game = {
   emptySpaces: 0,
   openedSpaces: 0,
   blocks: [],
-  state: 'running',
-  nearbyMineCount: function nearbyMineCount(row, index) {
+  state: gameState.running,
+  nearbyMineCount: function nearbyMineCount(row, column) {
     let mineCount = 0;
     const startingRow = row !== 0 ? row - 1 : row;
     const endingRow = row + 1 < this.blocks.length ? row + 1 : row;
-    const startingIndex = index !== 0 ? index - 1 : index;
-    const endingIndex = index + 1 < this.blocks[row].length
-      ? this.blocks[row].length - 1 : index;
+    const startingColumn = column !== 0 ? column - 1 : column;
+    const endingColumn = column + 1 < this.blocks[row].length
+      ? this.blocks[row].length - 1 : column;
 
     for (let i = startingRow; i <= endingRow; i += 1) {
-      for (let j = startingIndex; j <= endingIndex; j += 1) {
+      for (let j = startingColumn; j <= endingColumn; j += 1) {
         mineCount += this.blocks[i][j].isMine;
       }
     }
@@ -34,6 +46,7 @@ const game = {
       case 'click': {
         const block = this.blocks[data.row][data.index];
 
+        if (this.state === gameState.end) return;
         if (block.isOpened) return;
         if (block.isMarked) return;
 
@@ -41,16 +54,18 @@ const game = {
         this.openedSpaces += 1;
 
         if (block.isMine) {
-          element.innerHTML = 'you lost';
-          this.state = 'ended';
+          element.innerHTML = gameText.lose;
+          this.state = gameState.end;
           return;
         }
 
+        /* TODO: Handle 0 mines nearby
+         *   - should open other 0 mine block if any */
         block.element.innerHTML = this.nearbyMineCount(data.row, data.index);
 
         if (this.openedSpaces === this.emptySpaces) {
-          element.innerHTML = 'you won';
-          this.state = 'ended';
+          element.innerHTML = gameText.win;
+          this.state = gameState.end;
         }
         break;
       }
@@ -71,13 +86,13 @@ const row2 = document.createElement('DIV');
 const container1 = document.createElement('DIV');
 const container2 = document.createElement('DIV');
 
-element.innerHTML = 'Click blocks and try to avoid mines.';
+element.innerHTML = gameText.start;
 
 container1.classList.add('content__wrapper');
 container2.classList.add('content__wrapper');
 
 container1.appendChild(element);
-game.blocks = blockGrid(3, 3);
+game.blocks = blockGrid(10, 10);
 container2.appendChild(table1.build(game.blocks));
 
 row1.appendChild(container1);
